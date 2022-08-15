dce2ee52bbeb0563280863de681de153e947aaf0
SCollins <jaybuckco@gmail.com>
2022-08-15 19:37:45 +0000

    Finish spread operator. Add rest file

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ To use this repository:
 - Click the "Use this template" button in the upper right
 - Choose a name for your repository and create it
 - In your own repository, click the green "Gitpod" button
+
+# The Spread Operator
+
+To use this repository:
+- Click the "Use this template" button in the upper right
+- Choose a name for your repository and create it
+- In your own repository, click the green "Gitpod" button
+
+# Rest Parameter Syntax
+
+To use this repository:
+- Click the "Use this template" button in the upper right
+- Choose a name for your repository and create it
+- In your own repository, click the green "Gitpod" button
--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,49 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+   // Code block - executed when function called
+   // Can add params in parenthesis after function name
+   return a + b
+}
+
+let sum = addTwoNumbers(1, 7)
+console.log(sum)
 
 // Arrow Function With Parameters
+/** Usually defined as constants so can't be overwritten
+ * and must be defined before being called
+ */
+const addTwoNumbersTwo = (a, b) => {
+  // Code block
+  return a + b
+}
+let sumTwo = addTwoNumbersTwo(4, 9)
+console.log(sumTwo)
 
 // Single Line Arrow Function With Parameters
+// Shortened syntax
+const addTwoNumbersThree = (a, b) => a + b
+
+let sumThree = addTwoNumbersThree(15, 29)
+console.log(sumThree)
 
 // Implicit Returns
+const saySomething = message => console.log(message)
+
+saySomething("Hello friend.")
+
+const sayHello = () => console.log("Hello")
+sayHello()
 
 // Returning Multiple Lines
+/** When an arrow function needs to return a multiline piece of code
+ * parenthesis are required
+*/
+const returnMultipleLines = () => (
+  `<p>
+    This is a multiline string!
+  <p>`
+)
+
+console.log(returnMultipleLines())
